5d2a397ee003462551640e01e27aaeafd0e3cb10
LuisRubenC <lrcastillo03@hotmail.com>
2025-05-29 00:40:47 -0600

    Agrego contenido de las ramas asociadas al main en Calificaciones.py

--- Calificaciones.py
+++ Calificaciones.py
@@ -1,20 +1,41 @@
+def calcular_promedio(lista):
+    return sum(lista) / len(lista)
+def evaluar_aprobacion(promedio):
+    return "Aprobado" if promedio >= 6 else "Reprobado
 
-num_estudiantes = int(input("¿Cuántos estudiantes quieres registrar? "))
-estudiantes = [None] * num_estudiantes
+estudiantes = []
+aprobados = 0
+reprobados = 0
 
-for i in range(num_estudiantes):
-    print(f"\nEstudiante {i + 1}:")
-    nombre = input("Nombre del estudiante: ")
+cantidad = int(input("¿Cuántos estudiantes deseas registrar? "))
 
-    calificaciones = [None] * 4
-    calificaciones[0] = nombre  
+for i in range(cantidad):
+    print(f"\n Estudiante {i + 1}:")
+    nombre = input("Nombre: ")
+    calificaciones = []
 
-    for j in range(3):
-        nota = float(input(f"Calificación de la materia {j + 1}: "))
-        calificaciones[j+1] = nota 
+for j in range(3):
+        nota = float(input(f"Calificación {j + 1}: "))
+        calificaciones.append(nota)
 
-    estudiantes[i] = calificaciones  
+    promedio = calcular_promedio(calificaciones)
+    estado = evaluar_aprobacion(promedio)
 
-print("\nDatos de los estudiantes registrados:")
-for i in range(num_estudiantes):
-    print(f"Nombre: {estudiantes[i][0]}, Calificaciones: {estudiantes[i][1:]}")
+    if estado == "Aprobado":
+        aprobados += 1
+    else:
+        reprobados += 1
+
+    estudiantes.append({
+        "Nombre": nombre,
+        "Calificaciones": calificaciones,
+        "Promedio": promedio,
+        "Estado": estado
+    })
+
+print("\n --- Resultados --- ")
+for est in estudiantes:
+    print(f"{est['nombre']}: Promedio = {est['promedio']:.2f}, Estado = {est['estado']}")
+
+print(f"\nTotal Aprobados: {aprobados}")
+print(f"Total Reprobados: {reprobados}")
